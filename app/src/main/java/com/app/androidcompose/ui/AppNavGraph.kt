53d545f84cd5d4ffbafad67a7162ce08c08e10d2
Long Do <Longdt57@gmail.com>
2025-01-13 20:25:28 +0900

    Move NavExt to Compose Module

--- app/src/main/java/com/app/androidcompose/ui/AppNavGraph.kt
+++ app/src/main/java/com/app/androidcompose/ui/AppNavGraph.kt
@@ -1,14 +1,8 @@
 package com.app.androidcompose.ui
 
-import androidx.compose.animation.AnimatedContentScope
 import androidx.compose.runtime.Composable
-import androidx.navigation.NavBackStackEntry
-import androidx.navigation.NavGraphBuilder
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
-import androidx.navigation.navDeepLink
-import leegroup.module.compose.ui.models.BaseDestination
 import com.app.androidcompose.ui.screens.main.mainNavGraph
 
 @Composable
@@ -21,34 +15,5 @@ fun AppNavGraph(
         startDestination = AppDestination.MainNavGraph.destination
     ) {
         mainNavGraph(navController = navController)
-    }
-}
-
-fun NavGraphBuilder.composable(
-    destination: BaseDestination,
-    content: @Composable AnimatedContentScope.(NavBackStackEntry) -> Unit,
-) {
-    composable(
-        route = destination.route,
-        arguments = destination.arguments,
-        deepLinks = destination.deepLinks.map {
-            navDeepLink {
-                uriPattern = it
-            }
-        },
-        content = content
-    )
-}
-
-fun NavHostController.navigateWithDestination(destination: BaseDestination) {
-    when (destination) {
-        is BaseDestination.Up -> {
-            destination.results.forEach { (key, value) ->
-                previousBackStackEntry?.savedStateHandle?.set(key, value)
-            }
-            navigateUp()
-        }
-
-        else -> navigate(route = destination.destination)
     }
 }
--- app/src/main/java/com/app/androidcompose/ui/screens/main/MainNavGraph.kt
+++ app/src/main/java/com/app/androidcompose/ui/screens/main/MainNavGraph.kt
@@ -5,11 +5,10 @@ import androidx.navigation.NavHostController
 import androidx.navigation.compose.composable
 import androidx.navigation.navigation
 import com.app.androidcompose.ui.AppDestination
-import leegroup.module.compose.ui.models.BaseDestination
-import com.app.androidcompose.ui.composable
-import com.app.androidcompose.ui.navigateWithDestination
 import com.app.androidcompose.ui.screens.main.gituser.GitUserListScreen
 import com.app.androidcompose.ui.screens.main.gituserdetail.GitUserDetailScreen
+import leegroup.module.compose.support.extensions.appNavigate
+import leegroup.module.compose.support.extensions.composable
 
 fun NavGraphBuilder.mainNavGraph(
     navController: NavHostController,
@@ -24,19 +23,12 @@ fun NavGraphBuilder.mainNavGraph(
                 navigator = { destination -> navController.appNavigate(destination) }
             )
         }
-        composable<MainDestination.GitUserDetail.GitUserDetailNav> { backStackEntry ->
+        composable<MainDestination.GitUserDetail.GitUserDetailNav> {
             GitUserDetailScreen(
                 navigator = { destination ->
                     navController.appNavigate(destination)
                 }
             )
         }
-    }
-}
-
-fun NavHostController.appNavigate(destination: Any) {
-    when (destination) {
-        is BaseDestination -> navigateWithDestination(destination)
-        else -> navigate(destination)
     }
 }
